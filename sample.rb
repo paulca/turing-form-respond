4939265dded17653b74c7df9bef24a8f9501e880
Paul Campbell <paul@rslw.com>
2018-08-01 17:59:43 +0100

    Use the CSS from our widget
    Because there's enough to go around!

--- sample.rb
+++ sample.rb
@@ -1,11 +1,23 @@
 require "sinatra"
 require "http"
 
-get "/thanks/:response_id" do
+def get_response_from_remote_host
   @thanks = HTTP.get("#{ENV['REMOTE_HOST'] || 'http://localhost:9394'}/responses/#{params[:response_id]}").parse
+end
+
+def show_thankyou_page
   erb :thanks
 end
 
-get "/" do
+def show_index_page
   erb :index
+end
+
+get "/thanks/:response_id" do
+  get_response_from_remote_host
+  show_thankyou_page
+end
+
+get "/" do
+  show_index_page
 end
